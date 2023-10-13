68965b277f626ac4c66a1d6b1fd8be006c1e7b26
Yotsuya <yotsuya.dreams@gmail.com>
2023-10-13 13:20:11 +0200

    Add gRPC functionality to connect to web module

--- main.go
+++ main.go
@@ -3,53 +3,39 @@ package main
 import (
 	"context"
 	"fmt"
+	"gomosaics"
 	"log"
 	"net"
-	"os"
-	"time"
 
+	"github.com/google/uuid"
 	"google.golang.org/grpc"
 
-	pb "mosaics-grpc/proto"
+	pb "mosaics-processing-service/proto"
 )
 
-type HelloWorldServerImpl struct {
-	pb.UnimplementedHelloWorldServer
+type FileProcessingServerImpl struct {
+	pb.UnimplementedFileProcessingServer
 }
 
-func (s HelloWorldServerImpl) SayHello(c context.Context, req *pb.HelloReq) (*pb.HelloRepl, error) {
-	return &pb.HelloRepl{Msg: fmt.Sprintf("Hello there, %s!!!!", req.GetName())}, nil
-}
-
-func askForHello(client pb.HelloWorldClient) {
-	c, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	val, _ := client.SayHello(c, &pb.HelloReq{Name: "Ytsy"})
-	log.Println("Received a response: ", val)
+func (s FileProcessingServerImpl) ProcessFile(c context.Context, req *pb.ProcessFileReq) (*pb.ProcessFileRepl, error) {
+	inputFilepath := fmt.Sprintf("../mosaics-web/%s", req.Filepath)
+	outputFilepath := fmt.Sprintf("%s.png", uuid.NewString())
+	err := gomosaics.Mosaicate(inputFilepath, "../go-mosaics/icons_small/", outputFilepath, 4, 16)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ProcessFileRepl{
+		Filepath: outputFilepath,
+	}, nil
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("No args provided")
+	log.Println("Running mosaics-processing-service server")
+	lis, err := net.Listen("tcp", "0.0.0.0:8081")
+	if err != nil {
+		log.Fatal("Could not bind network interface: ", err)
 	}
-
-	mode := os.Args[1]
-	if mode == "client" {
-		log.Println("Running as client")
-		conn, err := grpc.Dial("localhost:8081")
-		if err != nil {
-			log.Fatal("An error occured: ", err)
-		}
-		defer conn.Close()
-		client := pb.NewHelloWorldClient(conn)
-		askForHello(client)
-	} else if mode == "server" {
-		log.Println("Running as server")
-		lis, _ := net.Listen("tcp", "localhost:8081")
-		srv := grpc.NewServer()
-		pb.RegisterHelloWorldServer(srv, HelloWorldServerImpl{})
-		srv.Serve(lis)
-	} else {
-		log.Fatal("Must run either as client or as server")
-	}
+	srv := grpc.NewServer()
+	pb.RegisterFileProcessingServer(srv, FileProcessingServerImpl{})
+	srv.Serve(lis)
 }
