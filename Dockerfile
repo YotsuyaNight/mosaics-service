f43bcbfc03acecf2aea84490c66950e1d0929789
Yotsuya <yotsuya.dreams@gmail.com>
2023-10-15 20:54:51 +0200

    Make some params configurable from env

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM golang:1.21.1
 WORKDIR /app
 COPY . .
+ENV BASE_DIR="/data"
+ENV RESULT_DIR="result"
+ENV ICONS_DIR="icons"
 RUN ["go", "build", "-o", "app"]
 EXPOSE 8081
 CMD ["./app"]
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"gomosaics"
 	"log"
 	"net"
+	"os"
+	"path"
 
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
@@ -13,19 +14,48 @@ import (
 	pb "mosaics-processing-service/proto"
 )
 
+func getEnvOr(name string, def string) string {
+	env := os.Getenv(name)
+	if env == "" {
+		return def
+	}
+	return env
+}
+
+func getBaseDir() string {
+	return getEnvOr("BASE_DIR", ".")
+}
+
+func getResultDir() string {
+	return getEnvOr("RESULT_DIR", "result")
+}
+
+func getIconsDir() string {
+	return getEnvOr("ICONS_DIR", "icons")
+}
+
+func prepareDirs() {
+	os.MkdirAll(path.Join(getBaseDir(), getIconsDir()), os.ModePerm)
+	os.MkdirAll(path.Join(getBaseDir(), getResultDir()), os.ModePerm)
+}
+
 type FileProcessingServerImpl struct {
 	pb.UnimplementedFileProcessingServer
 }
 
 func (s FileProcessingServerImpl) ProcessFile(c context.Context, req *pb.ProcessFileReq) (*pb.ProcessFileRepl, error) {
-	inputFilepath := fmt.Sprintf("../mosaics-web/%s", req.Filepath)
-	outputFilepath := fmt.Sprintf("%s.png", uuid.NewString())
-	err := gomosaics.Mosaicate(inputFilepath, "../go-mosaics/icons_small/", outputFilepath, 4, 16)
+	inputFilepath := path.Join(getBaseDir(), req.Filepath)
+	outputFilename := uuid.NewString() + ".png"
+	outputRelFilepath := path.Join(getResultDir(), outputFilename)
+	outputFilepath := path.Join(getBaseDir(), outputRelFilepath)
+	iconsDir := path.Join(getBaseDir(), getIconsDir())
+
+	err := gomosaics.Mosaicate(inputFilepath, iconsDir, outputFilepath, 4, 16)
 	if err != nil {
 		return nil, err
 	}
 	return &pb.ProcessFileRepl{
-		Filepath: outputFilepath,
+		Filepath: outputRelFilepath,
 	}, nil
 }
 
@@ -35,6 +65,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Could not bind network interface: ", err)
 	}
+	prepareDirs()
 	srv := grpc.NewServer()
 	pb.RegisterFileProcessingServer(srv, FileProcessingServerImpl{})
 	srv.Serve(lis)
